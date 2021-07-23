a3bd613437190f417d91cb96a08456595e26f33e
Chen Peng <pizer.chen@gmail.com>
2021-07-23 22:41:48 +0800

    fix minor errors

--- build.bat
+++ build.bat
@@ -5,3 +5,4 @@ docker build --rm -t bde2020/hadoop-resourcemanager:2.0.0-hadoop3.3.1-java8 ./re
 docker build --rm -t bde2020/hadoop-nodemanager:2.0.0-hadoop3.3.1-java8 ./nodemanager
 docker build --rm -t bde2020/hadoop-historyserver:2.0.0-hadoop3.3.1-java8 ./historyserver
 docker build --rm -t bde2020/hadoop-submit:2.0.0-hadoop3.3.1-java8 ./submit
+docker build --rm -t bde2020/hadoop-nginx:2.0.0-hadoop3.3.1-java8 ./nginx
--- wordcount.bat
+++ wordcount.bat
@@ -3,7 +3,7 @@ set ENV_FILE=hadoop.env
 set DOCKER_IMAGE_VERSION=2.0.0-hadoop3.3.1-java8
 docker build -t hadoop-wordcount ./submit
 docker run --network %DOCKER_NETWORK% --env-file %ENV_FILE% bde2020/hadoop-base:%DOCKER_IMAGE_VERSION% hdfs dfs -mkdir -p /input/
-docker run --network %DOCKER_NETWORK% --env-file %ENV_FILE% bde2020/hadoop-base:%DOCKER_IMAGE_VERSION% hdfs dfs -copyFromLocal -f /opt/hadoop-3.2.1/README.txt /input/
+docker run --network %DOCKER_NETWORK% --env-file %ENV_FILE% bde2020/hadoop-base:%DOCKER_IMAGE_VERSION% hdfs dfs -copyFromLocal -f /opt/hadoop-3.3.1/README.txt /input/
 docker run --network %DOCKER_NETWORK% --env-file %ENV_FILE% hadoop-wordcount
 docker run --network %DOCKER_NETWORK% --env-file %ENV_FILE% bde2020/hadoop-base:%DOCKER_IMAGE_VERSION% hdfs dfs -cat /output/*
 docker run --network %DOCKER_NETWORK% --env-file %ENV_FILE% bde2020/hadoop-base:%DOCKER_IMAGE_VERSION% hdfs dfs -rm -r /output
